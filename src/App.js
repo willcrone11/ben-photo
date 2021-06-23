6358e39b9caa2a3e05095cdb412db374f4557771
willcrone11 <72156281+willcrone11@users.noreply.github.com>
2021-06-23 16:51:22 -0400

    Adds routing to project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Home from './components/Home';
+import About from './components/About';
+import Blog from './components/Blog';
+import Contact from './components/Contact';
+import Portfolio from './components/Portfolio';
+import Post from './components/Post';
+import SinglePost from './components/SinglePost';
+import Pricing from './components/Pricing';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <BrowserRouter>
+    <Navbar />
+      <Switch>
+        <Route 
+          component={Home}
+          path='/'
+          exact
+        />
+        <Route 
+          component={About}
+          path='/about'
+        />
+        <Route 
+          component={Contact}
+          path='/contact'
+        />
+        <Route 
+          component={Portfolio}
+          path='/portfolio'
+        />
+        <Route 
+          component={Blog}
+          path='/blog'
+        />
+        <Route 
+          component={Pricing}
+          path='/pricing'
+        />
+        <Route 
+          component={SinglePost}
+          path='/post/:slug'
+        />
+        <Route 
+          component={Post}
+          path='/post'
+        />
+      </Switch>
+    </BrowserRouter>
+  )
 }
 
 export default App;
