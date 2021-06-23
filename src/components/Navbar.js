b9f44477b5c10808528db1f5b28288a087ed07ae
willcrone11 <72156281+willcrone11@users.noreply.github.com>
2021-06-23 19:10:58 -0400

    Updates logo, favicon, and navbar stylings

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -18,11 +18,11 @@ function classNames(...classes) {
 
 export default function Navbar() {
   return (
-    <Disclosure as="nav" className="bg-gray-800">
+    <Disclosure as="nav" className="bg-gray-00">
       {({ open }) => (
         <>
           <div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
-            <div className="relative flex items-center justify-between h-16">
+            <div className="relative flex items-center justify-between h-20">
               <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
                 {/* Mobile menu button*/}
                 <Disclosure.Button className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
@@ -39,19 +39,19 @@ export default function Navbar() {
                   <NavLink to="/" exact>
                     <img
                       src={Logo} 
-                      style={{width: 150, height: 60 }} 
+                      style={{width: 200, height: 30 }} 
                       alt="logo"
                     />
                   </NavLink>
                 </div>
-                <div className="hidden sm:block sm:ml-6">
+                <div className="hidden sm:block sm:ml-10">
                   <div className="flex space-x-4">
                     {navigation.map((item) => (
                       <a
                         key={item.name}
                         href={item.href}
                         className={classNames(
-                          item.current ? 'nav-link bg-gray-900 text-white' : 'nav-link text-gray-300 hover:bg-gray-700 hover:text-white',
+                          item.current ? 'nav-link bg-gray-900 text-white' : 'nav-link text-gray-900 hover:bg-gray-700 hover:text-white',
                           'px-3 py-2 rounded-md text-sm font-medium'
                         )}
                         aria-current={item.current ? 'page' : undefined}
@@ -72,7 +72,7 @@ export default function Navbar() {
                   key={item.name}
                   href={item.href}
                   className={classNames(
-                    item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
+                    item.current ? 'bg-gray-900 text-white' : 'text-gray-900 hover:bg-gray-700 hover:text-white',
                     'block px-3 py-2 rounded-md text-base font-medium'
                   )}
                   aria-current={item.current ? 'page' : undefined}
