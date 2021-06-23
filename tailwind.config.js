afba1c60bb48ad8e5b041581b24743cefd2f7c88
willcrone11 <72156281+willcrone11@users.noreply.github.com>
2021-06-23 16:51:13 -0400

    Adds navbar and tailwind stylings to project

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  purge: [
+    './src/components/Home',
+    './src/components/About',
+    './src/components/Blog',
+    './src/components/Contact',
+    './src/components/Portfolio',
+    './src/components/Pricing',
+    './src/components/Post',
+    './src/components/SinglePost',
+    './src/components/Navbar'
+  ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
