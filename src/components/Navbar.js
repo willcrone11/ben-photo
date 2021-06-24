8082d95c0b767f4515b0ee235670fe85d53a44bf
willcrone11 <72156281+willcrone11@users.noreply.github.com>
2021-06-23 21:35:44 -0400

    Updates navbar to underline active item

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,19 +3,6 @@ import Logo from '../img/ben-photo-logo.png';
 import { MenuIcon, XIcon } from '@heroicons/react/outline'
 import { NavLink } from 'react-router-dom';
 
-const navigation = [
-  { name: 'Home', href: '/', current: false },
-  { name: 'About', href: '/about', current: false },
-  { name: 'Pricing', href: '/pricing', current: false },
-  { name: 'Portfolio', href: '/portfolio', current: false },
-  { name: 'Blog', href: '/blog', current: false },
-  { name: 'Contact', href: '/contact', current: false },
-]
-
-function classNames(...classes) {
-  return classes.filter(Boolean).join(' ')
-}
-
 export default function Navbar() {
   return (
     <Disclosure as="nav" className="bg-gray-00">
@@ -46,19 +33,54 @@ export default function Navbar() {
                 </div>
                 <div className="hidden sm:block sm:ml-10">
                   <div className="flex space-x-4">
-                    {navigation.map((item) => (
-                      <a
-                        key={item.name}
-                        href={item.href}
-                        className={classNames(
-                          item.current ? 'nav-link bg-gray-900 text-white' : 'nav-link text-gray-900 hover:bg-gray-700 hover:text-white',
-                          'px-3 py-2 rounded-md font-medium'
-                        )}
-                        aria-current={item.current ? 'page' : undefined}
-                      >
-                        {item.name}
-                      </a>
-                    ))}
+                    <NavLink
+                      to="/"
+                      exact
+                      className="nav-link text-gray-900 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md font-medium"
+                      activeClassName="underline"
+                    >
+                      Home
+                    </NavLink>
+                    <NavLink
+                      to="/about"
+                      exact
+                      className="nav-link text-gray-900 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md font-medium"
+                      activeClassName="underline"
+                    >
+                      About
+                    </NavLink>
+                    <NavLink
+                      to="/pricing"
+                      exact
+                      className="nav-link text-gray-900 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md font-medium"
+                      activeClassName="underline"
+                    >
+                      Pricing
+                    </NavLink>
+                    <NavLink
+                      to="/portfolio"
+                      exact
+                      className="nav-link text-gray-900 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md font-medium"
+                      activeClassName="underline"
+                    >
+                      Portfolio
+                    </NavLink>
+                    <NavLink
+                      to="/blog"
+                      exact
+                      className="nav-link text-gray-900 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md font-medium"
+                      activeClassName="underline"
+                    >
+                      Blog
+                    </NavLink>
+                    <NavLink
+                      to="/contact"
+                      exact
+                      className="nav-link text-gray-900 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md font-medium"
+                      activeClassName="underline"
+                    >
+                      Contact
+                    </NavLink>
                   </div>
                 </div>
               </div>
@@ -67,19 +89,54 @@ export default function Navbar() {
 
           <Disclosure.Panel className="sm:hidden">
             <div className="px-2 pt-2 pb-3 space-y-1">
-              {navigation.map((item) => (
-                <a
-                  key={item.name}
-                  href={item.href}
-                  className={classNames(
-                    item.current ? 'bg-gray-900 text-white' : 'text-gray-900 hover:bg-gray-700 hover:text-white',
-                    'block px-3 py-2 rounded-md text-base font-medium'
-                  )}
-                  aria-current={item.current ? 'page' : undefined}
-                >
-                  {item.name}
-                </a>
-              ))}
+              <NavLink
+                to="/"
+                exact
+                className="nav-link text-gray-900 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md font-medium"
+                activeClassName="underline"
+              >
+                Home
+              </NavLink>
+              <NavLink
+                to="/about"
+                exact
+                className="nav-link text-gray-900 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md font-medium"
+                activeClassName="underline"
+              >
+                About
+              </NavLink>
+              <NavLink
+                to="/pricing"
+                exact
+                className="nav-link text-gray-900 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md font-medium"
+                activeClassName="underline"
+              >
+                Pricing
+              </NavLink>
+              <NavLink
+                to="/portfolio"
+                exact
+                className="nav-link text-gray-900 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md font-medium"
+                activeClassName="underline"
+              >
+                Portfolio
+              </NavLink>
+              <NavLink
+                to="/blog"
+                exact
+                className="nav-link text-gray-900 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md font-medium"
+                activeClassName="underline"
+              >
+                Blog
+              </NavLink>
+              <NavLink
+                to="/contact"
+                exact
+                className="nav-link text-gray-900 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md font-medium"
+                activeClassName="underline"
+              >
+                Contact
+              </NavLink>
             </div>
           </Disclosure.Panel>
         </>
