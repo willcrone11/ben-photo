dc9eb0d4ddf94996ed10629dad4b8a9924e167c6
willcrone11 <72156281+willcrone11@users.noreply.github.com>
2021-06-25 13:11:54 -0400

    Adds single post page for blog

--- src/components/Post.js
+++ src/components/Post.js
@@ -23,7 +23,7 @@ export default function Post() {
   });
 
   return (
-    <main className="bg-green-100 min-h-screen p-12"> 
+    <main className="bg-gray-200 min-h-screen p-12"> 
       <section className="container mx-auto">
         <h1 className="text-5xl flex justify-center cursive">Blog Posts</h1>
         <br/>
