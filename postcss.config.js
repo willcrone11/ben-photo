ab4001faf62103772bd4a511711b44bc3cb832a0
willcrone11 <72156281+willcrone11@users.noreply.github.com>
2021-07-02 12:09:26 -0400

    removes purge css rules

--- postcss.config.js
+++ postcss.config.js
@@ -1,16 +1,7 @@
-const purgecss = require('@fullhuman/postcss-purgecss')
-const cssnano = require('cssnano')
-
 module.exports = {
   plugins: [
+    require('postcss-import'),
     require('tailwindcss'),
     require('autoprefixer'),
-    cssnano({
-      preset: 'default'
-    }),
-    purgecss({
-      content: ['./public/*.html'],
-      defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
-    })
   ]
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,5 @@
 module.exports = {
-  purge: [
-    './src/components/Home',
-    './src/components/About',
-    './src/components/Blog',
-    './src/components/Contact',
-    './src/components/Portfolio',
-    './src/components/Pricing',
-    './src/components/Post',
-    './src/components/SinglePost',
-    './src/components/Navbar'
-  ],
+  purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
