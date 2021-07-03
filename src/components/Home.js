1fab69614b40aedd5573eb44378f2a55f1eff279
willcrone11 <72156281+willcrone11@users.noreply.github.com>
2021-07-03 11:46:21 -0400

    Updates homepage background

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import Portrait from '../img/wedding-portrait.jpeg';
+import homepageBackground from '../img/homepage-background.jpeg';
 import HomeInfo from './HomeInfo';
 
 export default function Home() {
   return (
     <main>
       <div className="lg:mb-72 md:mb-72 mb-20">
-        <img src={Portrait} alt="portrait" className="lg:fixed md:fixed absolute bg-fixed z-0" />
+        <img src={homepageBackground} alt="portrait" className="lg:fixed md:fixed absolute bg-fixed z-0" />
         <section className="relative opacity-80 justify-center content-center items-center pt-12  px-8">
           <div className="bg-white lg:mt-10 lg:p-12 justify-center content-center items-center text-center z-10">
             <h1 className="2xl:text-8xl lg:text-6xl text-4xl cursive text-black leading-none lg:leading-snug home-name">
